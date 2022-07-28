2e667a39d1a72db3be045aaef90ee1417c61c376
Kirill Semenov <kirik@mail.ru>
2022-07-28 21:59:30 +0300

    main: clear code

--- src/main/java/ru/devstend/getqr/GetQrApplication.java
+++ src/main/java/ru/devstend/getqr/GetQrApplication.java
@@ -1,6 +1,5 @@
 package ru.devstend.getqr;
 
-import java.io.BufferedReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class GetQrApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(GetQrApplication.class, args);
-    StringBuilder stringBuilder = new StringBuilder();
   }
 
 }
