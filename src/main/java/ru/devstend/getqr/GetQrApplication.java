12163a433284a950553a3e951cee5b960d32f907
Kirill Semenov <kirik@mail.ru>
2022-07-28 22:29:13 +0300

    main: added decorate k8s

--- src/main/java/ru/devstend/getqr/GetQrApplication.java
+++ src/main/java/ru/devstend/getqr/GetQrApplication.java
@@ -1,8 +1,13 @@
 package ru.devstend.getqr;
 
+import io.dekorate.kubernetes.annotation.ImagePullPolicy;
+import io.dekorate.kubernetes.annotation.Ingress;
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.annotation.Port;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@KubernetesApplication
 @SpringBootApplication
 public class GetQrApplication {
 
